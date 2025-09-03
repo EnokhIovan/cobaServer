c262a7fc6878e920486d72f0b9df0b4b7954fd68
EnokhIovan <jacobjovan26@gmail.com>
2025-09-03 19:08:35 +0700

    first design

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,20 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 8080;
 
-app.use('/pages', express.static(path.join(__dirname, 'pages')));
-app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
-app.use('/styles', express.static(path.join(__dirname, 'styles')));
+app.use('/pages', express.static(path.join(__dirname, '../frontend/pages')));
+app.use('/scripts', express.static(path.join(__dirname, '../frontend/scripts')));
+app.use('/styles', express.static(path.join(__dirname, '../frontend/styles')));
 
 app.get('/', (req, res) => {
+    res.redirect('/login');
+})
+
+app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/pages/login.html"));
+})
+
+app.get('/tugas', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/pages/tugas.html"));
 })
 
 app.listen(PORT, () => {
